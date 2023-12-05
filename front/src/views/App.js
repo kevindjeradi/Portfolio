467b117a840ea127fe896171af06084665e8a3ea
kevin <kevin-hamza.djeradi@epitech.eu>
2023-12-05 01:02:40 +0100

    created ComicBookBioTeaser route in app.jso

--- front/src/views/App.js
+++ front/src/views/App.js
@@ -4,6 +4,7 @@ import HomePage from 'views/HomePage';
 import VideoPage from 'views/VideoPage';
 import Experiences from 'views/Experiences';
 import Bio from 'views/Bio';
+import ComicBookBio from 'views/ComicBookBio';
 
 function App() {
       return (
@@ -13,6 +14,8 @@ function App() {
                               path="/" element={<HomePage />}/>
                         <Route
                               path="/Bio" element={<Bio />}/>
+                        <Route
+                              path="/ComicBookBio" element={<ComicBookBio />}/>
                         <Route
                               path="/projects" element={<Projects />}/>
                         <Route
