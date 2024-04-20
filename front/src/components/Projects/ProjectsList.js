412d9bb193d8ec159eba8a998aa32ca6538cea2b
kevin <kevin-hamza.djeradi@epitech.eu>
2024-04-21 01:45:04 +0200

    trying to fix video height on vps server

--- front/src/components/Projects/ProjectsList.js
+++ front/src/components/Projects/ProjectsList.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import ImageList from '@mui/material/ImageList';
 import Button from '@mui/material/Button';
 import ImageListItem from '@mui/material/ImageListItem';
